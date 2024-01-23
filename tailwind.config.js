ec7b6df4ab662b8cab807102717b49ca6a758366
azazahmad08 <engr.azaz08@gmail.com>
2024-01-23 17:59:19 +0600

    bg added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'white': '#ffffff', // You can use any color value for white
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+}
